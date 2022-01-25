bfed2220d0644011d09a020e9a2dc4e6ad84c1ca
matsimats <93705015+matsimats@users.noreply.github.com>
2022-01-25 10:27:53 +0100

    Update python.py

--- python.py
+++ python.py
@@ -1,35 +1,22 @@
-# Wymaga zainstalaowania bibliotek bs4
-# Importujemy BeautifulSoup class z modułu bs4
+
 from bs4 import BeautifulSoup
   
-# Otwarcie pliku HTML aplikacja.html
-HTMLFile = open("kontakty.html", "r", encoding="utf8")
-  
-# Odczyt
+HTMLFile = open("name_of_your_file.html", "r", encoding="utf8")
+
 index = HTMLFile.read()
-  
-# Tworzymy obiekt BeautifulSoup i definiujemy parser
+
 S = BeautifulSoup(index, 'lxml')
 
-# Lista do trzymania wartości
 list=[]
 
-# Szukamy Div'ów
 tag_name = S.find_all('div')
-for tag in tag_name:
-    # Szukamy Senchatestów wewnątrz Divów
-    if 'senchatest' in str(tag):
-        tag=str(tag)
-        # Rozdzielenie Senchatest
-        sencha = tag.partition("senchatest=")[2]
-        # Wyciągamy wartości przed "senchatest="
-        sencha = sencha.split("\"")[1]
-        # Dodanie do listy
-        list.append(sencha)
-'''
-Listujemy:
-print('senchatest=blaBlaBla')
-'''
 
+for tag in tag_name:
+    if 'tag_name=' in str(tag): # define your custom tag name
+        tag=str(tag)
+        sencha = tag.partition("tag_name=")[2] # and here
+        sencha = sencha.split("\"")[1]
+        list.append(sencha)
+        
 for i in list:
     print(i)
